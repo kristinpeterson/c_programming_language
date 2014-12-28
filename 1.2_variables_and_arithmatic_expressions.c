3bf7a24c02be5b429c1b94f271676b4c2770d505
kristinpeterson <kristinpeterson@me.com>
2014-12-28 23:39:08 +0000

    1.2 - exercise 1-3: add table header & exercise 1-4: add corresponding Celsius-Farenheit conversion table

--- 1.2_variables_and_arithmatic_expressions.c
+++ 1.2_variables_and_arithmatic_expressions.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /* print Farenheit-Celsius table
-	for fahr = 0, 20, ..., 300; floating-point version */
+	for fahr = 0, 20, ..., 300; floating-point version 
+   Then print corresponding Celsius-Tarenheit table */
 main() {
 	float fahr, celsius;
 	int lower, upper, step;
@@ -11,9 +12,23 @@ main() {
 	step = 20;	/* step size */
 	
 	fahr = lower;
+	
+	printf("Farenheit-Celsius Table\n");
+	printf("=======================\n");
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
+
+	// reset celsius value
+	celsius = 0;
+
+	printf("\nCelsius-Farenheit Table\n");
+	printf("=========================\n");
+	while (celsius <= upper) {
+		fahr = ((celsius * 9.0) / 5.0) + 32.0;
+		printf("%3.0f\t%6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}	
 }
