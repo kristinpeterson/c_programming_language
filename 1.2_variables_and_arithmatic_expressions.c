07a3cb08dbaf4cd28f793c09ea3280b98225238e
kristinpeterson <kristinpeterson@me.com>
2014-12-28 23:27:30 +0000

    1.2 - use floating point arithmatic for more precise calculation of celsius values

--- 1.2_variables_and_arithmatic_expressions.c
+++ 1.2_variables_and_arithmatic_expressions.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /* print Farenheit-Celsius table
-	for fahr = 0, 20, ..., 300 */
+	for fahr = 0, 20, ..., 300; floating-point version */
 main() {
-	int fahr, celsius;
+	float fahr, celsius;
 	int lower, upper, step;
 	
 	lower = 0;	/* lower limit of temperature table */
@@ -12,8 +12,8 @@ main() {
 	
 	fahr = lower;
 	while (fahr <= upper) {
-		celsius = 5 * (fahr-32) / 9;
-		printf("%3d\t%6d\n", fahr, celsius);
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr = fahr + step;
 	}
 }
